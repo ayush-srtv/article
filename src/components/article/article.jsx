1513c2aeb9599b99a3613f9658f88d8e715616bc
Ayush Srivastava <ayushs.1992@gmail.com>
2020-09-06 23:30:40 +0530

    Added intersection observer.

--- src/components/article/article.jsx
+++ src/components/article/article.jsx
@@ -1,8 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function Article({ children, component: Component = "article", ...props }) {
-  useEffect(() => {}, []);
-  return <Component {...props}>{children}</Component>;
+  const [articleRef, setArticleRef] = useState();
+  useEffect(() => {
+    let observer;
+    let didCancel = false;
+    if (articleRef && IntersectionObserver) {
+      observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (
+              !didCancel &&
+              (entry.intersectionRatio > 0 || entry.isIntersecting)
+            ) {
+            }
+          });
+        },
+        {
+          threshold: 0.01,
+          rootMargin: "75%"
+        }
+      );
+    }
+
+    return () => {
+      didCancel = true;
+      // on component cleanup, we remove the listner
+      if (observer && observer.unobserve) {
+        observer.unobserve(articleRef);
+      }
+    };
+  }, [articleRef]);
+  return (
+    <Component ref={setArticleRef} {...props}>
+      {children}
+    </Component>
+  );
 }
 
 export default Article;
